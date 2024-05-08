636a0709fd7c407cfa04e85dc26213c8c9f5d7ec
Kuba Bienia <135448047+bieniadev@users.noreply.github.com>
2024-05-08 14:19:16 +0200

    textfield for inputing 66 digit code

---
+++ szadogp/lib/components/input_code.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CodeInput extends StatelessWidget {
+  final TextEditingController controller;
+  const CodeInput({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(100),
+              gapPadding: 5,
+              borderSide:
+                  BorderSide(color: Theme.of(context).primaryColor, width: 4)),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(100),
+              gapPadding: 5,
+              borderSide: const BorderSide(color: Colors.black, width: 4)),
+          fillColor: Theme.of(context).primaryColor.withOpacity(0.4),
+          filled: true,
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 24, horizontal: 40),
+          labelStyle: const TextStyle(fontSize: 30, color: Colors.white),
+          floatingLabelAlignment: FloatingLabelAlignment.center,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          label: const Text('PODAJ KOD'),
+        ),
+        autocorrect: false,
+        style: const TextStyle(
+            color: Colors.white, fontSize: 30, letterSpacing: 25),
+        maxLength: 6,
+        showCursor: false,
+      ),
+    );
+  }
+}
