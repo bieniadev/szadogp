b058eb8796fcb24b1b40b2c42065e74a16ceec21
Kuba Bienia <135448047+bieniadev@users.noreply.github.com>
2024-05-08 13:40:15 +0200

    for calling actions on app

---
+++ szadogp/lib/components/action_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ActionButton extends StatelessWidget {
+  const ActionButton({super.key, required this.onTap, required this.hintText});
+
+  final Function()? onTap;
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        margin: const EdgeInsets.symmetric(horizontal: 30.0),
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(colors: [
+            Color.fromARGB(255, 160, 50, 199),
+            Color.fromARGB(255, 73, 19, 128)
+          ]),
+          borderRadius: BorderRadius.circular(100),
+          border: Border.all(color: Colors.black),
+        ),
+        child: Center(
+          child: Text(
+            hintText,
+            style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1),
+          ),
+        ),
+      ),
+    );
+  }
+}
