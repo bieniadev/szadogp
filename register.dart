73766bfbc929d463bc956a0c0602661629e542d2
Kuba Bienia <135448047+bieniadev@users.noreply.github.com>
2024-05-08 09:13:35 +0200

    Poprawa logo na register screen

---
+++ register.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:szadogp/components/input_textfield.dart';
+import 'package:szadogp/components/submit_button.dart';
+import 'package:szadogp/providers/current_screen.dart';
+import 'package:szadogp/screens/home.dart';
+import 'package:szadogp/screens/login.dart';
+
+class RegisterScreen extends ConsumerWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final usernameController = TextEditingController();
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+
+    //sign in user method
+    signUserIn(WidgetRef ref) {
+      print('Login: ${usernameController.text}');
+      print('Password: ${passwordController.text}');
+
+      //set current screen to home;
+      //   ref.read(currentScreenProvider.notifier).state = const HomeScreen();
+    }
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 100),
+                Image.asset('assets/images/logo.png'),
+                const SizedBox(height: 40),
+                const Text('Elo mordziaty, register swoj daj', style: TextStyle(color: Colors.white70)),
+                const SizedBox(height: 40),
+
+                //login input
+                InputTextfield(
+                  controller: usernameController,
+                  hintText: 'Username',
+                  obscureText: false,
+                ),
+                const SizedBox(height: 15),
+
+                //login input
+                InputTextfield(
+                  controller: emailController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+                const SizedBox(height: 15),
+
+                //password input
+                InputTextfield(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 20),
+
+                // create account text
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    GestureDetector(
+                      onTap: () => ref.read(currentScreenProvider.notifier).state = const LoginScreen(),
+                      child: const Text('Koxówa mordo dawaj login'),
+                    ),
+                  ]),
+                ),
+                const SizedBox(height: 30),
+
+                //sign in button
+                SubmitButton(
+                  onTap: () => signUserIn(ref),
+                  hintText: 'Zarejestruj się',
+                ),
+                const SizedBox(height: 30),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
