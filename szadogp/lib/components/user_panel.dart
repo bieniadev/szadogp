776412172b68e831ebd1093634f98362276c8d88
Kuba Bienia <135448047+bieniadev@users.noreply.github.com>
2024-05-08 15:50:48 +0200

    Dodanie nawigacji do ekranu statystyk uzytkownika

--- szadogp/lib/components/user_panel.dart
+++ szadogp/lib/components/user_panel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:szadogp/screens/user_stats.dart';
 
 class UserPanel extends StatelessWidget {
   const UserPanel({
@@ -7,24 +8,29 @@ class UserPanel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Color.fromARGB(255, 47, 41, 60),
-        borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(50.0),
-            bottomRight: Radius.circular(50.0)),
-      ),
-      child: const Padding(
-        padding: EdgeInsets.all(25.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            CircleAvatar(
-              radius: 30,
-            ),
-            Text('USERNAME', style: TextStyle(fontSize: 22)),
-            SizedBox(width: 60)
-          ],
+    return GestureDetector(
+      onTap: () => Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => const UserStatsScreen(),
+      )),
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 47, 41, 60),
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(50.0),
+              bottomRight: Radius.circular(50.0)),
+        ),
+        child: const Padding(
+          padding: EdgeInsets.all(25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CircleAvatar(
+                radius: 30,
+              ),
+              Text('USERNAME', style: TextStyle(fontSize: 22)),
+              SizedBox(width: 60)
+            ],
+          ),
         ),
       ),
     );
