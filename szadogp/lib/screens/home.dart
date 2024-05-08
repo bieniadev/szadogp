4a1e0c025ce01790c1291814310fe90fff4ec0ac
Kuba Bienia <135448047+bieniadev@users.noreply.github.com>
2024-05-08 15:07:12 +0200

    visual update

--- szadogp/lib/screens/home.dart
+++ szadogp/lib/screens/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:szadogp/components/action_button.dart';
+import 'package:szadogp/components/input_code.dart';
+import 'package:szadogp/components/user_panel.dart';
 import 'package:szadogp/providers/login_user.dart';
 
 class HomeScreen extends ConsumerWidget {
@@ -7,34 +10,39 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final userProvider = ref.watch(loginUserProvider);
+    // final userProvider = ref.watch(loginUserProvider);
+
+    final codeController = TextEditingController();
 
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Theme.of(context).primaryColor,
-        title: const Text('Home', selectionColor: Colors.white),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Center(
-          child: Column(
-            children: [
-              const Text('SIEMANOO'),
-              userProvider.when(
-                data: (userResponse) {
-                  String userToken = userResponse;
-                  return Text('Twój poufny token: $userToken');
-                },
-                error: (err, s) => Text(
-                  err.toString(),
-                ),
-                loading: (() => const CircularProgressIndicator()),
-              )
-            ],
-          ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const UserPanel(),
+            const SizedBox(height: 250),
+            Image.asset('assets/images/logo.png'),
+            const SizedBox(height: 60),
+            const Spacer(),
+            ActionButton(onTap: () {}, hintText: 'STWÓRZ GRĘ'),
+            const Spacer(),
+            CodeInput(controller: codeController),
+            const SizedBox(height: 40),
+          ],
         ),
       ),
     );
   }
 }
+
+
+
+// userProvider.when(
+//   data: (userResponse) {
+//     String userToken = userResponse;
+//     return Text('Twój poufny token: $userToken');
+//   },
+//   error: (err, s) => Text(
+//     err.toString(),
+//   ),
+//   loading: (() => const CircularProgressIndicator()),
+// )
