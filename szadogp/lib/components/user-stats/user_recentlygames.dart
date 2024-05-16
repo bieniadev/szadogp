ee00b46ce2e6ae53637444d9b1232932b9e6ef00
Kuba Bienia <000800@gippipgovpl.onmicrosoft.com>
2024-05-16 12:13:04 +0200

    poprawa ux dla 0 gier rozgeranych i pucharek emoji

--- szadogp/lib/components/user-stats/user_recentlygames.dart
+++ szadogp/lib/components/user-stats/user_recentlygames.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:szadogp/screens/game_details.dart';
 
 class UserRecentlyGames extends StatelessWidget {
@@ -8,84 +9,89 @@ class UserRecentlyGames extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(0),
-      itemCount: userStatsData.length,
-      itemBuilder: (context, index) {
-        List<dynamic> winnersTeam = userStatsData[index]['winnersGroup'];
-        return InkWell(
-          onTap: () => Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => GameDetailsScreen(gameStatsData: userStatsData[index]),
-          )),
-          child: Container(
-            color: index % 2 == 0 ? Colors.black.withOpacity(0.15) : Colors.black.withOpacity(0.25),
-            child: Row(
-              children: [
-                userStatsData[index]['isWinner'] // check for place
-                    ? Container(
-                        height: 80,
-                        width: 55,
-                        color: Colors.blue[400],
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text('W', style: TextStyle(fontSize: 20)),
-                            const Divider(
-                              color: Colors.white,
-                              thickness: 2,
-                              endIndent: 18,
-                              indent: 18,
+    return userStatsData.isEmpty
+        ? Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: Text('Tu pojawi się twoja historia gier', style: GoogleFonts.rubik(fontSize: 18)),
+          )
+        : ListView.builder(
+            padding: const EdgeInsets.all(0),
+            itemCount: userStatsData.length,
+            itemBuilder: (context, index) {
+              List<dynamic> winnersTeam = userStatsData[index]['winnersGroup'];
+              return InkWell(
+                onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => GameDetailsScreen(gameStatsData: userStatsData[index]),
+                )),
+                child: Container(
+                  color: index % 2 == 0 ? Colors.black.withOpacity(0.15) : Colors.black.withOpacity(0.25),
+                  child: Row(
+                    children: [
+                      userStatsData[index]['isWinner'] // check for place
+                          ? Container(
+                              height: 80,
+                              width: 55,
+                              color: Colors.blue[400],
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Text('W', style: TextStyle(fontSize: 20)),
+                                  const Divider(
+                                    color: Colors.white,
+                                    thickness: 2,
+                                    endIndent: 18,
+                                    indent: 18,
+                                  ),
+                                  Text(userStatsData[index]['time'], style: const TextStyle(fontSize: 12)),
+                                ],
+                              ),
+                            )
+                          : Container(
+                              height: 80,
+                              width: 55,
+                              color: Colors.red[400],
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Text('L', style: TextStyle(fontSize: 20)),
+                                  const Divider(
+                                    color: Colors.white,
+                                    thickness: 2,
+                                    endIndent: 18,
+                                    indent: 18,
+                                  ),
+                                  Text(userStatsData[index]['time'], style: const TextStyle(fontSize: 11)),
+                                ],
+                              ),
                             ),
-                            Text(userStatsData[index]['time'], style: const TextStyle(fontSize: 12)),
-                          ],
-                        ),
-                      )
-                    : Container(
-                        height: 80,
-                        width: 55,
-                        color: Colors.red[400],
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text('L', style: TextStyle(fontSize: 20)),
-                            const Divider(
-                              color: Colors.white,
-                              thickness: 2,
-                              endIndent: 18,
-                              indent: 18,
-                            ),
-                            Text(userStatsData[index]['time'], style: const TextStyle(fontSize: 11)),
-                          ],
+                      const SizedBox(width: 10),
+                      Container(
+                        clipBehavior: Clip.hardEdge,
+                        width: 150,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                        child: Image.network(userStatsData[index]['boardGameId']['imageUrl'], fit: BoxFit.cover, height: 70),
+                      ),
+                      const SizedBox(width: 8),
+                      Icon(Icons.emoji_events_sharp, size: 40, color: Colors.amber[400]), // to do: wstawic tu img pucharka
+                      const SizedBox(width: 8),
+                      Expanded(
+                        flex: 1,
+                        child: SizedBox(
+                          height: 23 * winnersTeam.length.toDouble(),
+                          width: 20,
+                          child: ListView.builder(
+                            padding: const EdgeInsets.all(0),
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: winnersTeam.length,
+                            itemBuilder: (context, index) => Text(winnersTeam[index]),
+                          ),
                         ),
                       ),
-                const SizedBox(width: 10),
-                Container(
-                  clipBehavior: Clip.hardEdge,
-                  width: 150,
-                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-                  child: Image.network(userStatsData[index]['boardGameId']['imageUrl'], fit: BoxFit.cover, height: 70),
-                ),
-                const SizedBox(width: 8),
-                const Text('WIN GR:'), // wstawic tu img pucharka
-                const SizedBox(width: 8),
-                Expanded(
-                  flex: 1,
-                  child: SizedBox(
-                    height: 23 * winnersTeam.length.toDouble(),
-                    width: 20,
-                    child: ListView.builder(
-                      padding: const EdgeInsets.all(0),
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: winnersTeam.length,
-                      itemBuilder: (context, index) => Text(winnersTeam[index]),
-                    ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
+              );
+            },
+          );
   }
 }
--- szadogp/lib/screens/user_stats.dart
+++ szadogp/lib/screens/user_stats.dart
@@ -4,6 +4,7 @@ import 'package:szadogp/components/user-stats/user_banner.dart';
 import 'package:szadogp/components/user-stats/user_banner_appbar.dart';
 import 'package:szadogp/components/user-stats/user_recentlygames.dart';
 import 'package:szadogp/components/user-stats/user_stats.dart';
+import 'package:szadogp/models/k_debug_data.dart';
 import 'package:szadogp/providers/user_data.dart';
 import 'package:szadogp/providers/user_stats.dart';
 
@@ -12,31 +13,45 @@ class UserStatsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final userData = ref.watch(userDataProvider);
+    // final userData = ref.watch(userDataProvider); //uncomment
     final userTestStats = ref.read(testUserStatsProvider);
     //to do: zmienic nasluchiwanie i ustawic request z api = lepsze rozwiazanie naprawi to dynamiczne sprawdzenie zakonczonej gry w statach
+    final userInfo = kDebugUserData;
 
     return Scaffold(
-      body: userData.when(
-          data: (userInfo) {
-            return Scaffold(
-                extendBodyBehindAppBar: true,
-                appBar: const UserBannerAppbar(),
-                body: Column(
-                  children: [
-                    //user banner with background
-                    UserBanner(username: userInfo['username']),
-                    // overal stats user/podsumowanie
-                    const UserStats(),
-                    // lista z recent played grami
-                    Expanded(child: UserRecentlyGames(userStatsData: userTestStats)),
-                  ],
-                ));
-          },
-          loading: () => const Center(child: CircularProgressIndicator()),
-          error: (err, s) {
-            return Text('$err');
-          }),
+      body: Scaffold(
+          extendBodyBehindAppBar: true,
+          appBar: const UserBannerAppbar(),
+          body: Column(
+            children: [
+              //user banner with background
+              UserBanner(username: userInfo['username']),
+              // overal stats user/podsumowanie
+              const UserStats(),
+              // lista z recent played grami
+              Expanded(child: UserRecentlyGames(userStatsData: userTestStats)),
+            ],
+          )),
+      // userData.when(
+      //     data: (userInfo) {
+      //       return Scaffold(
+      //           extendBodyBehindAppBar: true,
+      //           appBar: const UserBannerAppbar(),
+      //           body: Column(
+      //             children: [
+      //               //user banner with background
+      //               UserBanner(username: userInfo['username']),
+      //               // overal stats user/podsumowanie
+      //               const UserStats(),
+      //               // lista z recent played grami
+      //               Expanded(child: UserRecentlyGames(userStatsData: userTestStats)),
+      //             ],
+      //           ));
+      //     },
+      //     loading: () => const Center(child: CircularProgressIndicator()),
+      //     error: (err, s) {
+      //       return Text('$err');
+      //     }),
     );
   }
 }
