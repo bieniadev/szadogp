3e43f0d3fabecaf8fb6e4621c9e473c90f04adff
BuildTools <unconfigured@null.spigotmc.org>
2024-05-19 10:11:55 +0200

    widget structure fix

--- szadogp/lib/components/user-stats/dynamic_user_display.dart
+++ szadogp/lib/components/user-stats/dynamic_user_display.dart
@@ -1,21 +1,13 @@
-import 'dart:typed_data';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:szadogp/providers/selected_image.dart';
 
-class DynamicImageAvatar extends ConsumerStatefulWidget {
+class DynamicImageAvatar extends ConsumerWidget {
   const DynamicImageAvatar({super.key});
 
   @override
-  ConsumerState<DynamicImageAvatar> createState() => _DynamicImageAvatarState();
-}
-
-class _DynamicImageAvatarState extends ConsumerState<DynamicImageAvatar> {
-  Uint8List? _selectedImage;
-
-  @override
-  Widget build(BuildContext context) {
-    _selectedImage = ref.read(selectedImageProvider);
+  Container build(BuildContext context, WidgetRef ref) {
+    final selectedImage = ref.read(selectedImageProvider);
 
     return Container(
       height: 80,
@@ -24,7 +16,7 @@ class _DynamicImageAvatarState extends ConsumerState<DynamicImageAvatar> {
         borderRadius: BorderRadius.circular(25),
         border: Border.all(width: 2, color: Colors.black),
         color: Colors.white,
-        image: ref.watch(selectedImageProvider) == null ? null : DecorationImage(image: MemoryImage(_selectedImage!), fit: BoxFit.cover),
+        image: ref.watch(selectedImageProvider) == null ? null : DecorationImage(image: MemoryImage(selectedImage!), fit: BoxFit.cover),
       ),
     );
   }
